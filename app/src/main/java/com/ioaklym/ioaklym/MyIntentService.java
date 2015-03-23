4016790acbad166cb77b6981fd8d5761e9e46870
Maria Kwok <maria.ht.kwok@gmail.com>
2015-03-22 20:07:46 -0400

    Adding progression angle calculation and updating gitignore

--- app/src/main/java/com/ioaklym/ioaklym/MyIntentService.java
+++ app/src/main/java/com/ioaklym/ioaklym/MyIntentService.java
@@ -15,10 +15,16 @@ import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MyIntentService extends IntentService {
 
+    protected String hundreds;
+    protected String tens;
+    protected String ones;
+    protected String progression_angle;
+    protected Double reference_angle = 0.0;
     private static final String TAG = "MyIntentService";
 
     @Override
@@ -34,17 +40,41 @@ public class MyIntentService extends IntentService {
         HttpClient httpClient = new DefaultHttpClient();
         HttpPost httpPost = new HttpPost("https://ioaklym.herokuapp.com/steps");
         JSONObject json = new JSONObject();
-
+        String[] temp;
             try {
                 // JSON data:
                 Data jsonData = new Data();
                 jsonData.receiveData(bluetoothData);
+                temp = jsonData.getAngle().split("(?!^)");
+                if (reference_angle == 0.0) {
+                    reference_angle = Double.parseDouble(jsonData.getAngle());
+                } else if (Math.abs(Double.parseDouble(jsonData.getAngle()) - reference_angle) > 45.0) {
+                    reference_angle = Double.parseDouble(jsonData.getAngle());
+                }
+
+                if ((reference_angle - (Double.parseDouble(jsonData.getAngle()))) > 0.0 &&
+                        (reference_angle - (Double.parseDouble(jsonData.getAngle()))) < 10.0) {
+                    progression_angle = temp[temp.length-4] + "." + temp[temp.length-2] + temp[temp.length-1];
+                } else if ((reference_angle - (Double.parseDouble(jsonData.getAngle()))) >= 10.0 &&
+                        (reference_angle - (Double.parseDouble(jsonData.getAngle()))) < 20.0) {
+                    progression_angle = "1" + temp[temp.length-4] + "." + temp[temp.length-2] + temp[temp.length-1];
+                } else if ((reference_angle - (Double.parseDouble(jsonData.getAngle()))) < 0.0 &&
+                        (reference_angle - (Double.parseDouble(jsonData.getAngle()))) > -10.0) {
+                    progression_angle = "-" + temp[temp.length-4] + "." + temp[temp.length-1] + temp[temp.length-1];
+                } else if ((reference_angle - (Double.parseDouble(jsonData.getAngle()))) <= -10.0 &&
+                        (reference_angle - (Double.parseDouble(jsonData.getAngle()))) > -20.0) {
+                    progression_angle = "-1" + temp[temp.length-4] + "." + temp[temp.length-1] + temp[temp.length-1];
+                } else {
+                    progression_angle = temp[temp.length-4] + "." + temp[temp.length-2] + temp[temp.length-1];
+                }
+
                 Log.i(TAG, "Data sent for parsing and processing");
                 if (Double.parseDouble(jsonData.getStrideTime())>0.15){
                     // Ignore stride_time == 0.1s
                     json.put("user",jsonData.getUser());
-                    Log.d(TAG, "Angle: "+ jsonData.getAngle());
-                    json.put("angle",jsonData.getAngle());
+                    Log.d(TAG, "Raw Angle: "+ jsonData.getAngle());
+                    Log.d(TAG, "Progression Angle: "+ jsonData.getAngle());
+                    json.put("angle",progression_angle);
                     json.put("stride",jsonData.getStrideTime());
                     json.put("swing",jsonData.getSwingTime());
                     json.put("stance",jsonData.getStanceTime());
